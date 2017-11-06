83708d81767be54b072b556d8be1bba84ee34855
ciazhar <daisetsunakama@gmail.com>
2017-11-07 05:52:16 +0700

    initial commit

---
+++ src/main/java/com/example/springwebfluxannotationbased/SpringWebfluxAnnotationBasedApplication.java
@@ -0,0 +1,12 @@
+package com.example.springwebfluxannotationbased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxAnnotationBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxAnnotationBasedApplication.class, args);
+	}
+}
